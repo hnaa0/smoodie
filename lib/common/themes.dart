e1426fbe1919d2fb23f611f206207fb692393d14
unknown <hnaa12@daum.net>
2024-07-16 23:43:02 +0900

    Setup Theme

---
+++ lib/common/themes.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+abstract class SmoodieTheme {
+  static ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    fontFamily: "Pretendard",
+    useMaterial3: true,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      scrolledUnderElevation: 0,
+    ),
+    bottomSheetTheme: const BottomSheetThemeData(
+      elevation: 0,
+    ),
+    bottomAppBarTheme: const BottomAppBarTheme(
+      elevation: 0,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      filled: true,
+    ),
+    tabBarTheme: const TabBarTheme(
+      splashFactory: NoSplash.splashFactory,
+    ),
+  );
+  static ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    fontFamily: "Pretendard",
+    useMaterial3: true,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      scrolledUnderElevation: 0,
+    ),
+    bottomSheetTheme: const BottomSheetThemeData(
+      elevation: 0,
+    ),
+    bottomAppBarTheme: const BottomAppBarTheme(
+      elevation: 0,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      filled: true,
+    ),
+    tabBarTheme: const TabBarTheme(
+      splashFactory: NoSplash.splashFactory,
+    ),
+  );
+}
