9af27de263d506f872a002ad1c9641e53edaf114
unknown <hnaa12@daum.net>
2024-07-16 21:56:48 +0900

    Setup Theme colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,17 @@
+class SmoodieColors {
+  static const gray_100 = 0xffF2F2F2;
+  static const gray_200 = 0xffCCCCCC;
+  static const gray_300 = 0xffAFAFAF;
+  static const gray_400 = 0xff7F7F7F;
+  static const gray_500 = 0xff6C6C6C;
+  static const gray_600 = 0xff595959;
+  static const gray_700 = 0xff434343;
+  static const gray_800 = 0xff323232;
+  static const gray_900 = 0xff252525;
+
+  static const atomicTangerine = 0xffFFAC81;
+  static const coralPink = 0xffFF928B;
+  static const apricot = 0xffFEC3A6;
+  static const vanilla = 0xffEFE9AE;
+  static const teaGreen = 0xffCDEAC0;
+}
